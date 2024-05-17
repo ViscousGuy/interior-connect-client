de127415876c668b2cb769589832a746aab76b7e
viscousguy <shubhamanchaliya84@gmail.com>
2024-05-17 13:11:27 +0530

    feat: furniture search functionality

--- src/pages/Furniture/index.jsx
+++ src/pages/Furniture/index.jsx
@@ -9,14 +9,29 @@ import Spinner from "../../components/components/Spinner";
 // import GoToTop from "../../components/components/GoToTop";
 import Button from "../../components/components/Button";
 import { MdArrowBack } from "react-icons/md";
+import { BiSearch } from "react-icons/bi";
 
 const Furniture = () => {
   const { furnitures, isLoading } = useAppSelector((state) => state.furniture);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredFurniture, setFilteredFurniture] = useState([]);
+
   useEffect(() => {
     dispatch(getFurnitures());
   }, []);
+  useEffect(() => {
+    const filterFurniture = () => {
+      const filtered = furnitures.filter((furniture) =>
+        furniture.Name.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredFurniture(filtered);
+    };
+
+    filterFurniture();
+  }, [searchQuery, furnitures]);
+
   if (isLoading) return <Spinner />;
   return (
     <div className={`${styles.container} main-container`}>
@@ -24,10 +39,23 @@ const Furniture = () => {
         <Button className={styles.iconContainer} onClick={() => navigate(-1)}>
           <MdArrowBack className={styles.icon} />
         </Button>
-        <div className={styles.title}>{navData.find(item => item.value === "furnitures").name}</div>
+        <div className={styles.title}>
+          {navData.find((item) => item.value === "furnitures").name}
+        </div>
       </div>
+      <div className={styles.searchContainer}>
+        <BiSearch className={styles.searchIcon} />
+        <input
+          type="text"
+          placeholder="Search furniture"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className={styles.searchInput}
+        />
+      </div>
+
       <div className={styles.productList}>
-        {furnitures?.map((furniture) => {
+        {filteredFurniture?.map((furniture) => {
           return (
             <FurnitureCard
               key={furniture.Id}
